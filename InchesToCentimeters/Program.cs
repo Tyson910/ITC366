ba62dd7fca515d3e660d0a4e62fa4e2e33dbeefb
Tyson910 <Tyson910@lve.missouristate.edu>
2021-06-07 18:58:59 -0500

    HW1 create InchesToCentimeter program

---
+++ InchesToCentimeters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InchesToCentimeters
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            const float cmPerInch = 2.54f;
+            int inches = 3;
+            Console.WriteLine(inches + " inches is " + (inches * cmPerInch) + " centimeters");
+        }
+    }
+}
