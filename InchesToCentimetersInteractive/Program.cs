41c90f8ff52ac729f5d3c142143e3d8902c8a223
Tyson910 <Tyson910@lve.missouristate.edu>
2021-06-07 19:16:19 -0500

    HW1 remove ConsoleWriteLine

--- InchesToCentimetersInteractive/Program.cs
+++ InchesToCentimetersInteractive/Program.cs
@@ -7,7 +7,6 @@ namespace InchesToCentimetersInteractive
         public static void Main(string[] args)
         {
             const float cmPerInch = 2.54f;
-            //Console.WriteLine("Please enter a value in inches");
             Console.Write("Please enter a value in inches >>>  ");
             int inches = int.Parse( Console.ReadLine() );
             Console.WriteLine(inches + " inches is " + (inches * cmPerInch) + " centimeters");
