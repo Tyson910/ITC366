aeadcf75a8d432fb2a3789cafe6b938ebad465ad
Tyson910 <Tyson910@lve.missouristate.edu>
2021-06-07 19:13:47 -0500

    HW1 create InchesToCentimetersInteractive file

---
+++ InchesToCentimetersInteractive/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InchesToCentimetersInteractive
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            const float cmPerInch = 2.54f;
+            //Console.WriteLine("Please enter a value in inches");
+            Console.Write("Please enter a value in inches >>>  ");
+            int inches = int.Parse( Console.ReadLine() );
+            Console.WriteLine(inches + " inches is " + (inches * cmPerInch) + " centimeters");
+        }
+    }
+}
