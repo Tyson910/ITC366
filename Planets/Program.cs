b22c3619440167ef283af6b160916e9842e45676
Tyson910 <Tyson910@lve.missouristate.edu>
2021-06-08 22:17:26 -0500

    HW1 create Planet finder app

---
+++ Planets/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Planets
+{
+    class MainClass
+    {
+        enum Planets
+        {
+            Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a position in the solar system (1-8)");
+            int userPosition = int.Parse( Console.ReadLine() ) - 1;
+            Console.Write("You selected....");
+            Console.WriteLine( (Planets)userPosition );
+        }
+    }
+}
