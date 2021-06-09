d164c417172e95c93743151de2b28f9f2ff5522a
Tyson910 <Tyson910@lve.missouristate.edu>
2021-06-08 22:06:06 -0500

    HW1 fix syntax errors

---
+++ HW1_ex5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HW1_ex5
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            string name;
+            string firstString, secondString;
+            int first, second, product;
+            Console.Write("Enter your name >> ");
+            name = Console.ReadLine();
+            Console.Write("Hello, {0}! Enter an integer >> ", name);
+            firstString = Console.ReadLine();
+            first = int.Parse(firstString);
+            Console.Write("Enter another integer >> ");
+            secondString = Console.ReadLine();
+            second = int.Parse(secondString);
+            product = first * second;
+            Console.WriteLine("Thank you, {0}. The product of {1} and {2} is {3}",
+            name, first, second, product);
+        }
+
+    }
+}
