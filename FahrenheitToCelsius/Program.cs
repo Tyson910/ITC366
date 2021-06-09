9f1316adbe443478115e136556a776ad41442218
Tyson910 <Tyson910@lve.missouristate.edu>
2021-06-08 21:57:25 -0500

    HW1 create tempConverter app

---
+++ FahrenheitToCelsius/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FahrenheitToCelsius
+{
+    class MainClass
+    {
+        static float convertToC (float temp)
+        {
+            return (temp - 32) * 5/9f ;
+        }
+        public static void Main(string[] args)
+        {
+            Console.Write("Please enter a temperature in Fahrenheit >>>  ");
+            float userTemp = float.Parse( Console.ReadLine() );
+            float cTemp = convertToC(userTemp);
+            Console.WriteLine( userTemp.ToString("0.0") + " degrees F is " + cTemp.ToString("0.0") + " degrees C");
+        }
+    }
+}
