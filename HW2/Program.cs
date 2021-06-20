b11c314d0440be3fa96a8cedad517604e10f2a42
Tyson910 <Tyson910@lve.missouristate.edu>
2021-06-20 13:08:12 -0500

    HW2 solve exercise 1 and exercise 3

---
+++ HW2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace HW2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Exe1();
+            Exe3();
+        }
+        public static void Exe1()
+        {
+            string moState = "Missouri State University College of Business";
+            Console.WriteLine("Second Word : " + get2ndWord(moState));
+        }
+        private static string get2ndWord(string sentence)
+        {
+            int firstSpace = sentence.IndexOf(" ");
+            int secondSpace = sentence.IndexOf(" ", firstSpace + 1);
+            return sentence.Substring(firstSpace, secondSpace - firstSpace);
+        }
+        public static void Exe3()
+        {
+            string path = "The path is ";
+            Console.WriteLine(path + @"C:\Windows\System32");
+            Console.WriteLine(path + "C:\\Windows\\System32");
+        }
+    }
+}
