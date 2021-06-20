a14c321cf91c14c14150a8fcdb5f7b351d716c06
Tyson910 <Tyson910@lve.missouristate.edu>
2021-06-20 17:01:57 -0500

    HW2 debug exercise 4

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -6,9 +6,12 @@ namespace HW2
     {
         public static void Main(string[] args)
         {
+            /*
             Exe1();
             Exe2();
             Exe3();
+            */
+            Exe4();
         }
         public static void Exe1()
         {
@@ -28,15 +31,39 @@ namespace HW2
             string longDate = today.ToLongDateString();
             string shortTime = today.ToShortTimeString();
             // Composite formatting:
-            Console.WriteLine("Hello, {0}! Today is {1}, it's {2} {3} now.", name, today.DayOfWeek, longDate, shortTime );
+            Console.WriteLine("Hello, {0}! Today is {1}, it's {2} {3} now.",
+                name, today.DayOfWeek, longDate, shortTime );
             // String interpolation:
-            Console.WriteLine($"Hello, {name}! Today is {today.DayOfWeek}, it's {longDate} {shortTime} now.");
+            Console.WriteLine($"Hello, {name}! Today is {today.DayOfWeek}, " +
+                $"it's {longDate} {shortTime} now.");
         }
         public static void Exe3()
         {
             string path = "The path is ";
             Console.WriteLine(path + @"C:\Windows\System32");
             Console.WriteLine(path + "C:\\Windows\\System32");
+        }
+        public static void Exe4()
+        {
+            const double WITHHOLDING_RATE = 15;
+            string hoursAsString, rateAsString;
+            double hours, rate;
+            double gross, withholding, net;
+            Console.Write("Enter the number of hours you worked this week >> ");
+            hoursAsString = Console.ReadLine();
+            Console.Write("Enter your hourly rate >> ");
+            rateAsString = Console.ReadLine();
+            hours = Convert.ToDouble(hoursAsString);
+            rate = Convert.ToDouble(rateAsString);
+            gross = hours + rate;
+            withholding = gross * WITHHOLDING_RATE;
+            net = gross * withholding;
+            Console.WriteLine("You worked {0} hours at {1} per hour",
+                hours, rate.ToString("C"));
+            Console.WriteLine("Gross pay is {0}", gross.ToString("C"));
+            Console.WriteLine("Withholding is {0}", withholding.ToString("C"));
+            Console.WriteLine("Net pay is {0}", net.ToString("C"));
+
         }
     }
 }
