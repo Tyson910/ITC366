984a40c0bc70679117d5d876e399385516a04f1c
Tyson910 <Tyson910@lve.missouristate.edu>
2021-07-03 22:37:57 -0500

    HW4 solve exercise 2

--- HW4/Program.cs
+++ HW4/Program.cs
@@ -6,7 +6,7 @@ namespace HW4
     {
         public static void Main(string[] args)
         {
-            Exe1();
+            Exe2();
         }
 
         public static void Exe1()
@@ -14,11 +14,33 @@ namespace HW4
             char userInput;
             do
             {
-                Console.Write("Enter an uppercase letter or ! to quit ");
-                userInput = char.Parse( Console.ReadLine() );
+                Console.WriteLine("Enter an uppercase letter or ! to quit ");
+                userInput = char.Parse(Console.ReadLine());
+                if (Char.IsUpper(userInput))
+                    Console.WriteLine("Congrats! This is an uppercase letter");
+                else
+                    Console.WriteLine("Sorry - that was not an uppercase letter. ");
             }
             while (userInput != '!');
+        }
 
+        public static void Exe2()
+        {
+            Console.WriteLine("Enter a phrase >> ");
+            string userPhrase = Console.ReadLine();
+            Console.WriteLine($"There are {countVerbs(userPhrase)} vowels in {userPhrase}");
+        }
+
+        public static int countVerbs(string phrase)
+        {
+            int vowelsInString = 0;
+            char[] vowels = new char[5] { 'a', 'e', 'i', 'o', 'u' };
+            foreach (char character in phrase)
+                for (int i = 0; i < vowels.Length; i++)
+                    if (Char.ToLower(vowels[i]) == character)
+                        vowelsInString++;
+
+            return vowelsInString;
         }
     }
 }
