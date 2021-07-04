3652a244d89ef90cd417474e5aa28d99f0f2e513
Tyson910 <Tyson910@lve.missouristate.edu>
2021-07-03 22:27:43 -0500

    HW4 solve exercise 1

---
+++ HW4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HW4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Exe1();
+        }
+
+        public static void Exe1()
+        {
+            char userInput;
+            do
+            {
+                Console.Write("Enter an uppercase letter or ! to quit ");
+                userInput = char.Parse( Console.ReadLine() );
+            }
+            while (userInput != '!');
+
+        }
+    }
+}
