6308ea29f5df688efe8443d8adac37e36e9ba974
Tyson910 <Tyson910@lve.missouristate.edu>
2021-07-18 11:41:00 -0500

    HW6 solve exercise 1

---
+++ HW6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HW6
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Exe1();
+        }
+
+        public static void Exe1()
+        {
+            Console.Write("Enter miles >> ");
+            int userMiles =  int.Parse( Console.ReadLine() ) ;
+            Console.WriteLine($" {userMiles} miles is { mi_to_km(userMiles) } kilometers");
+        }
+        public static float mi_to_km(int miles)
+        {
+            return (float)miles * (float)1.60934  * 1.0f ; 
+        }
+    }
+}
