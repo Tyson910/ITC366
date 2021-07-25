8866ccbe4cfb102b7687a46fc46432bc5606e8fe
Tyson910 <Tyson910@lve.missouristate.edu>
2021-07-25 16:31:50 -0500

    HW7 solve exercise 2

--- HW7/Program.cs
+++ HW7/Program.cs
@@ -6,20 +6,47 @@ namespace HW7
     {
         public static void Main(string[] args)
         {
-            Exe3();
+            Exe2();
+        }
+
+        private static void Exe2()
+        {
+            int i;
+            int height = 4;
+            int width = 3;
+            int len = 2;
+
+            const int ROOM_COUNT = 10;
+            Room[] rooms = new Room[ROOM_COUNT];
+            for (i = 0; i < rooms.Length; i++)
+            {
+                rooms[i] = new Room(height, width, len);
+                len += 3;
+                width += 5;
+                if ( i % 2 == 0)
+                    height += 2;
+            }
+
+            for (i = 0; i < rooms.Length; i++)
+            {
+                Room room = rooms[i];
+                Console.WriteLine($"For a {room.Height} X {room.Width} X {room.Len} ");
+                Console.WriteLine($"  Two walls are {room.Len} long and {room.Height} high ");
+                Console.WriteLine($"       and the other two walls are {room.Width} long and {room.Height} high ");
+                Console.WriteLine($"  Total wall area is {room.Area}, so you need {room.Gallons} gallon(s) of paint. \n");
+            }
         }
 
         private static void Exe3()
         {
-
-            const int SALES_COUNT = 2;
+            const int SALES_COUNT = 10;
             Sale[] sales = new Sale[SALES_COUNT];
             for (int i = 0; i < sales.Length; i++)
             {
                 sales[i] = new Sale(); 
-                Console.Write($"Enter inventory number #{i} >> ");
+                Console.Write($"Enter inventory number #{ i + 1} >> ");
                 sales[i].InventoryNum = int.Parse( Console.ReadLine() );
-                Console.Write($"Enter amount of sale >>");
+                Console.Write($"Enter amount of sale >> ");
                 sales[i].SaleAmount = double.Parse( Console.ReadLine() );                
             }
 
---
+++ HW7/Room.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace HW7
+{
+    public class Room
+    {
+        public int Height, Width, Len, Area, Gallons;
+
+        public Room(int height, int width, int len)
+        {
+            this.Height = height;
+            this.Width = width;
+            this.Len = len;
+            calcAreaOfRoom();
+            calcPaintNeeded();
+        }
+
+        private void calcAreaOfRoom()
+        {
+            int areaOfOneWall = Width * Len;
+            this.Area = areaOfOneWall * 4;
+        }
+
+        private void calcPaintNeeded()
+        {
+            this.Gallons = (Area / 350) + 1;
+        }
+    }
+}
